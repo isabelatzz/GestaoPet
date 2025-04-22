31e236d8cce1cf4ed52860b6f67e25b3c0a976d3
Jefferson Bezerra <jeffebezerram13@gmail.com>
2025-04-22 13:30:20 -0300

    Commit 22/04 - 13:30

---
+++ src/test/java/test/PetLoverMapTest.java
@@ -0,0 +1,169 @@
+package test;
+
+import ProjetoPetShop.entities.Animal;
+import ProjetoPetShop.entities.Tutor;
+import ProjetoPetShop.exception.TutorCadastrado;
+import ProjetoPetShop.system.petlover.PetLoverMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PetLoverMapTest {
+    private PetLoverMap sistema;
+    private Tutor tutorValido;
+    private Animal animalValido;
+
+
+    @BeforeEach
+    void setUp() {
+        sistema = new PetLoverMap();
+
+        tutorValido = new Tutor("Isabaaela", "155.197.224-50",
+                "83 99609189896", "Rua São João, Itapiririca-PB",
+                "belinhagames111@gmjdail.com");
+
+        animalValido = new Animal(1, "Belinha", "Gato",
+                "Siamês", 7, tutorValido);
+
+    }
+
+    @Test
+    void testCadastrarTutorComSucesso(){
+        sistema.cadastrarTutor(tutorValido);
+        Tutor tutorEncontrado = sistema.buscarTutorPorCpf(tutorValido.getCpf());
+
+        assertNotNull(tutorEncontrado,"Tutor deveria ser encontrado");
+        assertEquals(tutorValido.getCpf(),tutorEncontrado.getCpf(),"CPF não corresponde");
+    }
+
+
+
+    @Test
+    void testCadastrarTutorDuplicadoDeveLançarExceçao(){
+        sistema.cadastrarTutor(tutorValido);
+        assertThrows(TutorCadastrado.class, () -> sistema.cadastrarTutor(tutorValido),"Deveria lançar TutorCadastrado ao cadastrar tutor duplicado");
+
+
+    }
+
+    @Test
+    void testCadastrarAnimalComTutorNaoCadastrado(){
+        sistema.cadastrarAnimal(animalValido);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            sistema.cadastrarAnimal(animalValido);
+        }, "Deveria lançar exceção quando tutor não está cadastrado");
+    }
+
+    @Test
+    void testCadastrarAnimalComSucesso(){
+        sistema.cadastrarTutor(tutorValido);
+        sistema.cadastrarAnimal(animalValido);
+
+        Animal animalEncontrado = sistema.buscarAnimalPorID(animalValido.getCodigo());
+
+        assertNotNull(animalEncontrado, "Animal deveria ter sido cadastrado");
+        assertEquals(animalValido.getNome(), animalEncontrado.getNome(), "Nome do animal não corresponde");
+        assertEquals(tutorValido.getCpf(), animalEncontrado.getTutor().getCpf(), "Tutor do animal não corresponde");
+    }
+
+    @Test
+    void testBuscarAnimalPorIDInexistente(){
+        sistema.cadastrarTutor(tutorValido);
+        sistema.cadastrarAnimal(animalValido);
+
+        assertNotNull(sistema.buscarAnimalPorID(1), "Animal com ID=1 deveria existir!");
+    }
+
+    @Test
+    void testListarAnimaisPorTutor(){
+        sistema.cadastrarTutor(tutorValido);
+        sistema.cadastrarAnimal(animalValido);
+
+        Animal animal2 = new Animal(2, "Peixinho", "Cachorro",
+                "Vira-Lata", 2, tutorValido);
+        sistema.cadastrarAnimal(animal2);
+
+        List<Animal> animaisDoTutor = sistema.listarAnimaisPorTutor(tutorValido.getCpf());
+
+        assertEquals(2, animaisDoTutor.size(), "Deveria retornar 2");
+        assertTrue(animaisDoTutor.contains(animalValido), "Lista deveria conter o animal 1");
+        assertTrue(animaisDoTutor.contains(animal2), "Lista deveria conter o animal 2");
+    }
+
+    @Test
+    void testRemoverAnimal(){
+        sistema.cadastrarTutor(tutorValido);
+        sistema.cadastrarAnimal(animalValido);
+
+        int idAnimal = animalValido.getCodigo();
+        sistema.removerAnimal(idAnimal);
+
+        assertNull(sistema.buscarAnimalPorID(idAnimal), "Animal deveria ter sido removido");
+
+        // Verificar se foi removido da lista do tutor também
+        List<Animal> animaisDoTutor = sistema.listarAnimaisPorTutor(tutorValido.getCpf());
+        assertTrue(animaisDoTutor.isEmpty(), "Lista de animais deveria estar vazia");
+    }
+
+    @Test
+    void testAtualizarAnimal(){
+        sistema.cadastrarTutor(tutorValido);
+        sistema.cadastrarAnimal(animalValido);
+
+        Tutor novoTutor = new Tutor("Val", "123.567.334.90", "83 98854-2586",
+                "Rua São Paulo, Ita-PB", "valaraujo@gmail.com");
+        sistema.cadastrarTutor(novoTutor);
+
+        // Modificar o animal
+        animalValido.setNome("Rex Atualizado");
+        animalValido.setEspecie("Cachorro Atualizado");
+        animalValido.setRaca("Labrador Atualizado");
+        animalValido.setTutor(novoTutor);
+
+        sistema.atualizarAnimal(animalValido);
+
+        Animal animalAtualizado = sistema.buscarAnimalPorID(animalValido.getCodigo());
+
+        assertEquals("Rex Atualizado", animalAtualizado.getNome());
+        assertEquals("Cachorro Atualizado", animalAtualizado.getEspecie());
+        assertEquals("Labrador Atualizado", animalAtualizado.getRaca());
+        assertEquals(novoTutor.getCpf(), animalAtualizado.getTutor().getCpf());
+    }
+
+    @Test
+    void testListarTodosTutores(){
+        sistema.cadastrarTutor(tutorValido);
+
+        Tutor tutor2 = new Tutor("Jailson", "123.456.678-90", "83996091896",
+                "Rua São João, MME-PB", "jailson@gmail.com");
+        sistema.cadastrarTutor(tutor2);
+
+        List<Tutor> todosTutores = sistema.listarTodosTutores();
+
+        assertEquals(2, todosTutores.size());
+        assertTrue(todosTutores.contains(tutorValido));
+        assertTrue(todosTutores.contains(tutor2));
+    }
+
+    @Test
+    void testListarTodosAnimais(){
+        sistema.cadastrarTutor(tutorValido);
+        sistema.cadastrarAnimal(animalValido);
+
+        Animal animal2 = new Animal(2, "Jade", "Gato", "Siamês",
+                6, tutorValido);
+        sistema.cadastrarAnimal(animal2);
+
+        List<Animal> todosAnimais = sistema.listarTodosAnimais();
+
+        assertEquals(2, todosAnimais.size());
+        assertTrue(todosAnimais.contains(animalValido));
+        assertTrue(todosAnimais.contains(animal2));
+    }
+
+    }
+
---
+++ src/test/java/test/ServicoPetLoverMapTest.java
@@ -0,0 +1,136 @@
+package test;
+
+import ProjetoPetShop.entities.Animal;
+import ProjetoPetShop.entities.Servico;
+import ProjetoPetShop.entities.Tamanho;
+import ProjetoPetShop.entities.Tutor;
+import ProjetoPetShop.system.servico.ServicoPetLoverMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ServicoPetLoverMapTest {
+    private ServicoPetLoverMap sistema;
+    private Tutor tutorValido;
+    private Animal animalValido;
+    private Servico servicoValido;
+
+    @BeforeEach
+    void setUp(){
+        sistema = new ServicoPetLoverMap();
+
+        tutorValido = new Tutor("Isabela", "155090224-50", "83996091896",
+                "Rua São João,23,ITA-PB", "isabela@gmail.com");
+
+        animalValido = new Animal(3, "Bela", "Cachorro", "Pinscher", 9,
+                tutorValido);
+
+        servicoValido = new Servico(1, animalValido, 100.0, "Dr João", false,
+                Tamanho.MEDIO,10.0) {
+            @Override
+            protected String getDetalhesServico() {
+                return "Banho";
+            }
+
+            @Override
+            public double calcularValorTotal() {
+                return getValorBase() + calcularAcrescimo();
+            }
+        };
+    }
+
+    @Test
+    void testGerarRecibo(){
+        String recibo = servicoValido.gerarRecibo();
+        System.out.println("Conteúdo do recibo:\n" + recibo);
+
+        assertAll(
+                () -> assertTrue(recibo.contains("PetLover"), "Cabeçalho do recibo"),
+                () -> assertTrue(recibo.contains("Bela"), "Nome do animal"),
+                () -> assertTrue(recibo.contains("Isabela"), "Nome do tutor"),
+                () -> assertTrue(recibo.contains("R$ 100,00"), "Valor base"),
+                () -> assertTrue(recibo.contains("Banho"), "Detalhes específicos"),
+                () -> assertTrue(recibo.contains("PENDENTE"), "Status de pagamento")
+        );
+    }
+
+    @Test
+    void testCadastrarServicoComSucesso(){
+        sistema.cadastrarServico(servicoValido);
+
+        String recibo = sistema.gerarRecibo(servicoValido.getId());
+        assertNotNull(recibo);
+        assertTrue(recibo.contains("Banho"));
+    }
+
+    @Test
+    void testGerarReciboPDFComSucesso() {
+        sistema.cadastrarServico(servicoValido);
+
+        assertDoesNotThrow(() -> {
+            sistema.gerarReciboPDF(servicoValido.getId());
+        }, "Geração de PDF não deveria lançar exceção");
+    }
+
+    @Test
+    void testListarServiçosPendentes(){
+        Servico servicoPendente = new Servico(2, animalValido, 100.00,
+                "Dr Isadora", false,Tamanho.PEQUENO, 20.0)
+        {
+            @Override
+            protected String getDetalhesServico() {
+                return "Consulta";
+            }
+
+            @Override
+            public double calcularValorTotal() {
+                return getValorBase() + calcularAcrescimo();
+            }
+        };
+
+        sistema.cadastrarServico(servicoPendente);
+
+        List<Servico> pendentes = sistema.listarServicosPendentes();
+        assertEquals(1, pendentes.size(), "Deveria lançar 1 serviço pendente");
+        assertEquals("Consulta", pendentes.get(0).getDetalhes(), "Detalhes incorretos");
+    }
+
+    @Test
+    void testAdicionarServico(){
+        Servico novoServico = new Servico(3, animalValido, 75.00, "Dr Igor Meira", true,
+                Tamanho.MEDIO, 15.0)
+        {
+            @Override
+            protected String getDetalhesServico() {
+                return "Banho";
+            }
+
+            @Override
+            public double calcularValorTotal() {
+                return getValorBase() + calcularAcrescimo();
+            }
+        };
+
+        assertTrue(sistema.listarTodosServicos().isEmpty(),
+                "Deveria começar sem serviços");
+
+        sistema.adicionarServico(novoServico);
+        String recibo = sistema.gerarRecibo(novoServico.getId());
+
+        assertAll(
+                () -> assertEquals(1, sistema.listarTodosServicos().size(),
+                        "Deveria ter 1 serviço cadastrado"),
+
+                () -> assertEquals(1, novoServico.getId(),
+                        "Deveria ter atribuído ID automático"),
+
+                () -> assertTrue(recibo.contains("Banho"), "Recibo deveria conter detalhes do serviço"),
+
+                () -> assertEquals(86.25, novoServico.calcularValorTotal(), 0.01,
+                        "75 (base) + 11.25 (15%) = 86.25")
+        );
+    }
+}
